a7b50605da7b4e2d569ef836caf8b421bce5daa2
Veronica L <verolozada12@gmail.com>
2018-11-16 14:38:45 -0500

    html home and survey made

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+let friendsArr = [];
+
+
+// const info = {
+//     name: 'Veronica',
+//     age: 23
+// }
+// friends.push(info);
+// console.log(friends);
+// console.log(friends[0].name);
+
+module.exports = friendsArr;
