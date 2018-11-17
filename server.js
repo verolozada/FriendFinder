ed03146b17a053391c3fcbfbc4597bc9a135396b
Veronica L <verolozada12@gmail.com>
2018-11-17 11:09:20 -0500

    post working

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 //requiring routes files
 require("./app/routing/apiRoutes")(app)
 require("./app/routing/htmlRoutes")(app)
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.listen(PORT, () => {
     console.log(`Server http://localhost:${PORT}`);
-});
+})
 
