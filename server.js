83e7adb13763d5971a2860173f7fad9e91bbdd17
Veronica L <verolozada12@gmail.com>
2018-11-17 23:42:12 -0500

    changes to server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 //requiring routes files
 require("./app/routing/apiRoutes")(app)
 require("./app/routing/htmlRoutes")(app)
